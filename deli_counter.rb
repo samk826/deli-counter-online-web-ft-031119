e615f417641fd35e3db84d17364133983704808c
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-19 14:40:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,3 +15,12 @@ def take_a_number(katz_deli, name)
   katz_deli<<name
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts 
+# 3. Build the `now_serving` method which should call out (i.e. `puts`) the next
+# person in line and then remove them from the front. If there is nobody in line,
+#  it should call out (`puts`) that `"There is nobody waiting to be served!"`.
