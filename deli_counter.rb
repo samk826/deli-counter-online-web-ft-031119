4541dcc1c70cc41b2ddf428dd0599ea78db53f5b
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-19 14:58:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,13 @@
 def line(deli)
   if deli.empty?
 puts "The line is currently empty."
+else
+  deli_line="The line is currently:"
+  deli.each.with_index(1) do |name, i|
+  end
 end
 end
 
-# Build a method that a new customer will use when entering the deli. The `take_a_number`
-# method should accept two arguments, the array for the current line of people (`katz_deli`),
-# and a string containing the name of the person joining the end of the line. The method
-#  should call out (`puts`) the person's name along with their position in line.
-#   **Top-Tip:** *Remember that people like to count from* `1`*, not from* `0` *("zero") like compute
 
 
 def take_a_number(katz_deli, name)
@@ -24,6 +23,3 @@ def now_serving(katz_deli)
     katz_deli.shift
   end
 end
-# 3. Build the `now_serving` method which should call out (i.e. `puts`) the next
-# person in line and then remove them from the front. If there is nobody in line,
-#  it should call out (`puts`) that `"There is nobody waiting to be served!"`.
