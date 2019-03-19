11a2813ba0022e1f888e23b5514af98a8364f11d
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-19 14:41:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,9 @@ def now_serving(katz_deli)
   if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts 
+    puts
+  end
+end
 # 3. Build the `now_serving` method which should call out (i.e. `puts`) the next
 # person in line and then remove them from the front. If there is nobody in line,
 #  it should call out (`puts`) that `"There is nobody waiting to be served!"`.
